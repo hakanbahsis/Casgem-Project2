36fba96cb77f972a8ba10d3e2e50e6ba0142ce0b
hakanbahsis <hakan.bahsis@hotmail.com>
2023-07-12 12:18:26 +0300

    first commit

---
+++ Casgem_CodeFirstProject/Controllers/DefaultController.cs
@@ -0,0 +1,63 @@
+﻿using Casgem_CodeFirstProject.DAL.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Casgem_CodeFirstProject.Controllers
+{
+    public class DefaultController : Controller
+    {
+        TravelContext context = new TravelContext();
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        } 
+        
+        public PartialViewResult PartialHeader()
+        {
+            return PartialView();
+        } 
+        public PartialViewResult PartialSlider()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialBooking()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialSpecification()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialCovers()
+        {
+            var values = context.Destinations.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialTeams()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialClient()
+        {
+            return PartialView();
+        } 
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialScripts()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ Casgem_CodeFirstProject/DAL/Entities/SocialMedia.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Casgem_CodeFirstProject.DAL.Entities
+{
+    public class SocialMedia
+    {
+        public int SocialMediaId { get; set; }
+        public string Name { get; set; }
+        public string Url { get; set; }
+
+        public int GuideId { get; set; }
+        public Guide Guide { get; set; }
+    }
+}
